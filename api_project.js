f4469181f5b7c61116453df0860cf3b7e5ad6a11
Travis CI User <travis@example.org>
2018-02-02 17:47:25 +0000

    Updates

--- api_project.js
+++ api_project.js
@@ -10,7 +10,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-02-02T17:18:58.945Z",
+    "time": "2018-02-02T17:47:20.963Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
