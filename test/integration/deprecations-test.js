471adbc6765e0ab768057bc768118ceb3cabf5c1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-02-02 21:39:47 -0500

    mocha + Cypress against same tests

--- test/integration/deprecations-test.js
+++ test/integration/deprecations-test.js
@@ -1,39 +1,17 @@
+require('../mocha-options')
+
 const GitHub = require('../../')
 const nock = require('nock')
-const haveCypress = () =>
-  typeof Cypress === 'object'
-// not necessary in Cypress, but does not break
-const chai = require('chai')
-const expect = chai.expect
-// const simple = require('simple-mock')
-const sinon = require('sinon')
-const sinonChai = require('sinon-chai')
-chai.use(sinonChai)
-
-// const mocha = require('mocha')
-// describe = mocha.describe
-// it = mocha.it
-// beforeEach = mocha.beforeEach
-// afterEach = mocha.afterEach
 
 /* eslint-env mocha */
 /* global cy */
 /* eslint-disable no-unused-expressions */
 describe.only('deprecations', () => {
-  if (haveCypress()) {
-    beforeEach(() => {
-      cy.stub(console, 'warn')
-    })
-  } else {
-    beforeEach(() => {
-      sinon.stub(console, 'warn')
-    })
-    afterEach(() => {
-      console.warn.restore()
-    })
-  }
+  beforeEach(() => {
+    cy.stub(console, 'warn')
+  })
 
-  it('github.integrations.*', (done) => {
+  it('github.integrations.*', () => {
     nock('https://deprecations-test.com')
       .get('/app/installations')
       .reply(200, [])
@@ -41,12 +19,10 @@ describe.only('deprecations', () => {
     const github = new GitHub({
       host: 'deprecations-test.com'
     })
-    github.integrations.getInstallations({})
+    return github.integrations.getInstallations({})
     .then(() => {
-      // chai-expect is built into Cypress
-      // so this syntax is easier to have in Node and browser
       expect(console.warn).to.be.calledTwice
-    }).then(done)
+    })
   })
 
   it('deprecated followRedirects option', () => {
